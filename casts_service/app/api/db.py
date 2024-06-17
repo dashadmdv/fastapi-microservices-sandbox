e2601b169aff0407ab1de66f15cd861c642b9874
dashadmdv <apushkins7@gmail.com>
2024-06-18 01:34:49 +0300

    add: casts service

---
+++ casts_service/app/api/db.py
@@ -0,0 +1,24 @@
+# ~/python-microservices/cast-service/app/api/db.py
+
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, ARRAY
+
+from databases import Database
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+casts = Table(
+    "casts",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50)),
+    Column("nationality", String(20)),
+)
+
+database = Database(DATABASE_URL)
