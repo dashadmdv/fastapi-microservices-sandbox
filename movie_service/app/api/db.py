892129d997d51fead843cfb7bb28f02b9f93c7b4
dashadmdv <apushkins7@gmail.com>
2024-06-18 00:52:05 +0300

    add: movies service dockerfile

--- movie_service/app/api/db.py
+++ movie_service/app/api/db.py
@@ -1,10 +1,13 @@
 # ~/movie-service/app/api/db.py
+from dotenv import load_dotenv
+import os
 
 from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, ARRAY
 
 from databases import Database
 
-DATABASE_URL = "postgresql://postgres:12345678@localhost/movie_db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
