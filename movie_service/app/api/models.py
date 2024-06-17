de2afecc5c7fa0288eb9ac8df242796c0a3bb80a
dashadmdv <apushkins7@gmail.com>
2024-06-18 01:24:05 +0300

    add: movies service api versioning

--- movie_service/app/api/models.py
+++ movie_service/app/api/models.py
@@ -8,7 +8,7 @@ class MovieIn(BaseModel):
     name: str
     plot: str
     genres: List[str]
-    casts: List[str]
+    casts_id: List[int]
 
 
 class MovieOut(MovieIn):
@@ -19,4 +19,4 @@ class MovieUpdate(MovieIn):
     name: Optional[str] = None
     plot: Optional[str] = None
     genres: Optional[List[str]] = None
-    casts: Optional[List[str]] = None
+    casts_id: Optional[List[int]] = None
